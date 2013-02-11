848de649a1504842eda3857eedcae6bfcc1540b1
Priyanka Rani <priyanka.rani@openlabs.co.in>
2013-02-11 23:36:58 +0530

    A module to store static file to amazon s3 #816
    A module is added that will allow nereid static files to be stored on
    amazon-s3 bucket.
    
    Task ID: project-3/task-816
    Review ID: 9377011

---
+++ __init__.py
@@ -0,0 +1,9 @@
+'''
+    trytond_nereid_s3 init file
+
+    Tryton module to support Nereid-S3 for Amazon S3 storage
+
+:copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited.
+:license: GPLv3, see LICENSE for more details
+'''
+import static_file
